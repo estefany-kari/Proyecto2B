fba348dd3c4abd9e849282e01bdbfe1199c14e04
CinthiaBR <cinthia.burbano@epn.edu.ec>
2021-09-18 20:18:09 -0500

    menu de tareas, carview de las tareas y habitos, menu sanduche, cambio de color en menu

---
+++ app/src/main/java/com/example/projectv1/MenuSanduche.kt
@@ -0,0 +1,11 @@
+package com.example.projectv1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MenuSanduche : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_menu_sanduche)
+    }
+}
---
+++ app/src/main/java/com/example/projectv1/habito.kt
@@ -0,0 +1,3 @@
+package com.example.projectv1
+
+data class habito (val nombreHabito: String, val frecuenciaHabito: String, val imagenHabito:String )
