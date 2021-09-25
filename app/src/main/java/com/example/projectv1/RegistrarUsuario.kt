f7d2acd5bb236ba97726f264033f58fabc25f997
Estefany Canencia <estefany.canencia@epn.edu.ec>
2021-09-24 19:58:23 -0500

    new project

--- app/src/main/java/com/example/projectv1/RegistrarUsuario.kt
+++ app/src/main/java/com/example/projectv1/RegistrarUsuario.kt
@@ -79,7 +79,6 @@ class RegistrarUsuario : AppCompatActivity() {
         if (Password2 != Password) {
             editTextTextPassword2.setError("Las contraseñas no coinsiden")
         }
-
             progressBar.setVisibility(View.VISIBLE)
             Auth.createUserWithEmailAndPassword(correoUsuario, Password)
                 .addOnCompleteListener(this) { task ->
@@ -108,35 +107,6 @@ class RegistrarUsuario : AppCompatActivity() {
                                         ).show();
                                         progressBar.setVisibility(View.GONE);
                                     }
-
-        progressBar.setVisibility(View.VISIBLE)
-        Auth.createUserWithEmailAndPassword(correoUsuario, Password)
-            .addOnCompleteListener(this) { task ->
-                if (task.isSuccessful()) {
-                    val user = User(Nombre, correoUsuario, Fecha, Password, Password2);
-                    FirebaseAuth.getInstance().currentUser?.let {
-                        FirebaseDatabase.getInstance().getReference("Users")
-                            .child(it.uid)
-                            .setValue(user).addOnCompleteListener {
-
-                                if (task.isSuccessful()) {
-                                    Toast.makeText(
-                                        this,
-                                        "USUARIO REGISTRADO EXITOSAMENTE",
-                                        Toast.LENGTH_LONG
-                                    ).show();
-                                    progressBar.setVisibility(View.VISIBLE);
-                                    //DIRIGIR A LA VENTANA LOGIN
-                                    abrirActividad(MainActivity::class.java)
-
-                                } else {
-                                    Toast.makeText(
-                                        this,
-                                        "FALLO EL REGISTRO, INTENTALO NUEVAMENTE",
-                                        Toast.LENGTH_LONG
-                                    ).show();
-                                    progressBar.setVisibility(View.GONE);
-
                                 }
                         }
                     } else {
@@ -148,19 +118,7 @@ class RegistrarUsuario : AppCompatActivity() {
                         progressBar.setVisibility(View.GONE);
                     }
 
-
                 }
-
-                } else {
-                    Toast.makeText(
-                        this,
-                        "FALLO EL REGISTRO, INTENTALO NUEVAMENTE",
-                        Toast.LENGTH_LONG
-                    ).show();
-                    progressBar.setVisibility(View.GONE);
-                }
-
-            }
 
 
     }
@@ -168,6 +126,7 @@ class RegistrarUsuario : AppCompatActivity() {
         val datePicker = DatePickerFragment{ day, month, year -> OnDateSelected(day, month, year)}
         datePicker.show(supportFragmentManager, "datePcker")
     }
+
     fun OnDateSelected(day:Int, month:Int, year:Int){
         txtFecha.setText("${day}/${month}/${year}")
     }
@@ -191,20 +150,6 @@ class RegistrarUsuario : AppCompatActivity() {
         }
     }
 }
-
-        private fun validarPassword(): Boolean {
-            val Password = findViewById<EditText>(R.id.editTextTextPassword)
-
-            val passwordInput: String = Password.getText().toString().trim()
-            return  if (!PASSWORD_PATTERN.matcher(passwordInput).matches()) {
-                Password.setError("Contraseña muy debil")
-                false
-            } else {
-                Password.setError(null)
-                true
-            }
-        }
-    }
 
 
 
